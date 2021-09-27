b5f466dd215ddae0d2ddebf33e2c77a5e4663dde
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2021-09-27 09:53:08 +0200

    only use sudo where needed

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ LAPTOP_KEEB=/dev/input/by-path/platform-i8042-serio-0-event-kbd
 KEEB=$LAPTOP_KEEB
 
 set -ex
+# zig build -Drelease-safe
 zig build
 
-intercept -g $KEEB | zig-out/bin/layerz | uinput -d $KEEB
+sudo intercept -g $KEEB | zig-out/bin/layerz | sudo uinput -d $KEEB
